75ba47fbb5a79f9330e5fe4126023a6b5caee3d9
Ife Odugbesan <ifeodugbesan@yahoo.com>
2021-07-29 23:12:18 +0100

    added image parallax effect

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,12 @@ import "bootstrap";
 import "./cookie_bar"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
+import simpleParallax from 'simple-parallax-js';
 
 AOS.init();
+
+const image = document.querySelectorAll('.parallax-img');
+new simpleParallax(image, {
+  scale: 1.3,
+  overflow: true
+});
